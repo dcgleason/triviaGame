2291473a52a6f7de022333ebc21f9e36531a54e3
Danny Gleason <danny.c.gleason@gmail.com>
2019-12-10 16:52:43 -0500

    fixed bug, added progress bar

--- script.js
+++ script.js
@@ -17,148 +17,166 @@ const nasaURL = "https://api.nasa.gov/EPIC/api/natural/images?api_key="
 function backgroundImage(){
  let jpeg = ".jpg"
  
-    fetch(nasaURL+API_KEY)
-    .then(response => response.json())
-    .then(data => 
-    document.body.style.backgroundImage = `url('${data[0].image+jpeg}')`)
-    }
-
+fetch(nasaURL+API_KEY)
+ .then(response => response.json())
+ .then(data => 
+    // console.log(data[1].image)
+    document.body.style.backgroundImage = `url('https://epic.gsfc.nasa.gov/archive/natural/2019/6/27/png/${data[0].image}.png')`
+    )}
 backgroundImage()
 
 // display question and answers to page
 
 
-let question = document.querySelector('#question')
-let answerOne = document.querySelector("#answer-one")
-let answerTwo= document.querySelector("#answer-two")
-let answerThree = document.querySelector("#answer-three")
+let questionDiv = document.querySelector('#question')
+let answerOneDiv = document.querySelector("#answer-one")
+let answerTwoDiv= document.querySelector("#answer-two")
+let answerThreeDiv = document.querySelector("#answer-three")
 let result = document.querySelector("#result")
 let streak = document.querySelector("#winning-streak")
 let numRightInARow = 0
 let total = 0
+let progressBarFull = document.querySelector("#progressBarFull")
+const totalQuestions = 3
+let fraction = document.querySelector(".fraction")
 
+setScreen()
 
+function setScreen(){
+    progressBarFull.style.width = `${(total/totalQuestions) * 100}em`
+    fraction.innerHTML = "Questions:" + total + "/" + totalQuestions
+}
 
-//addeventlistener to buttons(if == correct, model pops up)
-
-firstTurn();
-
-function firstTurn(){
-
-question.innerHTML = trivia.firstQuestion
-answerOne.innerHTML = trivia.firstAnswer[0]
-answerTwo.innerHTML = trivia.firstAnswer[1]
-answerThree.innerHTML = trivia.firstAnswer[2]
-
-
-answerOne.addEventListener('click', function(){
-    result.innerHTML= "That was incorrect"
-    numRightInARow=0
-    streak.innerHTML = "Your winning streak is " + numRightInARow
-    secondTurn()
-})
-answerTwo.addEventListener('click', function(){
-    result.innerHTML= "That was correct"
-    numRightInARow++
-    total+=1
-    streak.innerHTML = "Your winning streak is " + numRightInARow
+  //addeventlistener to buttons(if == correct, model pops up)
+  firstTurn();
+  function firstTurnAnswer1() {
+    result.innerHTML = "That was incorrect";
+    numRightInARow = 0;
+    streak.innerHTML = "Your winning streak is " + numRightInARow;
+    fraction.innerHTML = "Questions: " + total + "/" + totalQuestions
     secondTurn();
-
-
+  }
+  function firstTurnAnswer2() {
+    result.innerHTML = "That was correct";
+    numRightInARow++;
+    console.log(total);
+    total += 1;
+    console.log(total);
+    streak.innerHTML = "Your winning streak is " + numRightInARow;
+    fraction.innerHTML = "Questions: " + total + "/" + totalQuestions
+    progressBarFull.style.width = `${(total/totalQuestions) * 100}px`
+    console.log(total/totalQuestions)
+    secondTurn();
     // input modal and then put in a new question / answer
-})
-answerThree.addEventListener('click', function(){
-    result.innerHTML= "That was incorrect"
-    numRightInARow=0
-    streak.innerHTML = "Your winning streak is " + numRightInARow
-    secondTurn()
-})
-}
-
-
-// function for second quesiotn
-
-function secondTurn(){
-
-question.innerHTML = trivia.secondQuestion
-answerOne.innerHTML = trivia.secondAnswer[0]
-answerTwo.innerHTML = trivia.secondAnswer[1]
-answerThree.innerHTML = trivia.secondAnswer[2]
-
-    answerOne.addEventListener('click', function(){
-        result.innerHTML= "That was correct"
-        numRightInARow++
-        console.log(total)
-        total+=1
-        console.log(total)
-        streak.innerHTML = "Your winning streak is " + numRightInARow
-        thirdTurn()
-    })
-    answerTwo.addEventListener('click', function(){
-        result.innerHTML= "That was incorrect"
-        numRightinARow=0 
-        streak.innerHTML = "Your winning streak is " + numRightInARow
-        thirdTurn()
- 
-    
-    })
-    answerThree.addEventListener('click', function(){
-        result.innerHTML= "That was incorrect"
-        numRightinARow=0
-        streak.innerHTML = "Your winning streak is " + numRightInARow
-        thirdTurn()
-    })
-    
-
-}
-//function for thrid question
-function thirdTurn(){
-question.innerHTML = trivia.thirdQuestion
-answerOne.innerHTML = trivia.thirdAnswer[0]
-answerTwo.innerHTML = trivia.thirdAnswer[1]
-answerThree.innerHTML = trivia.thirdAnswer[2]
-
-    answerOne.addEventListener('click', function(){
-        result.innerHTML= "That was incorrect"
-        numRightinARow = 0
-        console.log(total)
-        streak.innerHTML = "Your winning streak is " + numRightInARow
-        gameOver()
-    })
-    answerTwo.addEventListener('click', function(){
-        result.innerHTML= "That was correct"
-        console.log(total)
-        total+=1
-        console.log(total)
-        numRightInARow++
-        streak.innerHTML = "Your winning streak is " + numRightInARow
-        gameOver()
-
- 
-    })
-    answerThree.addEventListener('click', function(){
-        result.innerHTML= "That was incorrect"
-        numRightInARow=0
-        console.log(total)
-        streak.innerHTML = "Your winning streak is " + numRightInARow
-        gameOver()
-    })
-}
-
-
-function gameOver(){
-document.getElementById("title").innerHTML = "Game Over"
-question.innerHTML = ""
-answerOne.innerHTML = ""
-answerTwo.innerHTML = ""
-result.innerHTML = ""
-answerThree.innerHTML = ""
-streak.innerHTML="Your total score is " + total
-
-}
+  }
+  function firstTurnAnswer3() {
+    result.innerHTML = "That was incorrect";
+    numRightInARow = 0;
+    streak.innerHTML = "Your winning streak is " + numRightInARow;
+    fraction.innerHTML = "Questions: " + total + "/" + totalQuestions
+    secondTurn();
+  }
+  function secondTurnAnswer1() {
+    result.innerHTML = "That was correct";
+    numRightInARow++;
+    console.log(total);
+    total += 1;
+    console.log(total);
+    streak.innerHTML = "Your winning streak is " + numRightInARow;
+    fraction.innerHTML = "Questions: " + total + "/" + totalQuestions
+    progressBarFull.style.width = `${(total/totalQuestions) * 100}px`
+    console.log(total/totalQuestions)
+    thirdTurn();
+  }
+  function secondTurnAnswer2() {
+    result.innerHTML = "That was incorrect";
+    numRightinARow = 0;
+    streak.innerHTML = "Your winning streak is " + numRightInARow;
+    fraction.innerHTML = "Questions: " + total + "/" + totalQuestions
+    thirdTurn();
+  }
+  function secondTurnAnswer3() {
+    result.innerHTML = "That was incorrect";
+    numRightinARow = 0;
+    streak.innerHTML = "Your winning streak is " + numRightInARow;
+    fraction.innerHTML = "Questions: " + total + "/" + totalQuestions
+    thirdTurn();
+  }
+  function thirdTurnAnswer1() {
+    result.innerHTML = "That was incorrect";
+    numRightinARow = 0;
+    console.log(total);
+    streak.innerHTML = "Your winning streak is " + numRightInARow;
+    fraction.innerHTML = "Questions: " + total + "/" + totalQuestions
+    gameOver();
+  }
+  function thirdTurnAnswer2() {
+    result.innerHTML = "That was correct";
+    console.log(total);
+    total += 1;
+    console.log(total);
+    numRightInARow++;
+    streak.innerHTML = "Your winning streak is " + numRightInARow;
+    fraction.innerHTML = "Questions: " + total + "/" + totalQuestions
+    progressBarFull.style.width = `${(total/totalQuestions) * 100}px`
+    console.log(total/totalQuestions)
+    gameOver();
+  }
+  function thirdTurnAnswer3() {
+    result.innerHTML = "That was incorrect";
+    numRightInARow = 0;
+    console.log(total);
+    streak.innerHTML = "Your winning streak is " + numRightInARow;
+    fraction.innerHTML = "Questions: " + total + "/" + totalQuestions
+    gameOver();
+  }
+  function firstTurn() {
+    questionDiv.innerHTML = trivia.firstQuestion;
+    answerOneDiv.innerHTML = trivia.firstAnswer[0];
+    answerTwoDiv.innerHTML = trivia.firstAnswer[1];
+    answerThreeDiv.innerHTML = trivia.firstAnswer[2];
+    answerOneDiv.addEventListener("click", firstTurnAnswer1); // function () {}
+    answerTwoDiv.addEventListener("click", firstTurnAnswer2);
+    answerThreeDiv.addEventListener("click", firstTurnAnswer3);
+  }
+  // function for second quesiotn
+  function secondTurn() {
+    questionDiv.innerHTML = trivia.secondQuestion;
+    answerOneDiv.innerHTML = trivia.secondAnswer[0];
+    answerTwoDiv.innerHTML = trivia.secondAnswer[1];
+    answerThreeDiv.innerHTML = trivia.secondAnswer[2];
+    answerOneDiv.removeEventListener("click", firstTurnAnswer1);
+    answerTwoDiv.removeEventListener("click", firstTurnAnswer2);
+    answerThreeDiv.removeEventListener("click", firstTurnAnswer3);
+    answerOneDiv.addEventListener("click", secondTurnAnswer1);
+    answerTwoDiv.addEventListener("click", secondTurnAnswer2);
+    answerThreeDiv.addEventListener("click", secondTurnAnswer3);
+  }
+  //function for thrid questionDiv
+  function thirdTurn() {
+    questionDiv.innerHTML = trivia.thirdQuestion;
+    answerOneDiv.innerHTML = trivia.thirdAnswer[0];
+    answerTwoDiv.innerHTML = trivia.thirdAnswer[1];
+    answerThreeDiv.innerHTML = trivia.thirdAnswer[2];
+    answerOneDiv.removeEventListener("click", secondTurnAnswer1);
+    answerTwoDiv.removeEventListener("click", secondTurnAnswer2);
+    answerThreeDiv.removeEventListener("click", secondTurnAnswer3);
+    answerOneDiv.addEventListener("click", thirdTurnAnswer1);
+    answerTwoDiv.addEventListener("click", thirdTurnAnswer2);
+    answerThreeDiv.addEventListener("click", thirdTurnAnswer3);
+  }
+  function gameOver() {
+    document.getElementById("title").innerHTML = "Game Over";
+    questionDiv.innerHTML = "";
+    answerOneDiv.innerHTML = "";
+    answerTwoDiv.innerHTML = "";
+    result.innerHTML = "";
+    answerThreeDiv.innerHTML = "";
+    streak.innerHTML = "Your total score is " + total;
+  }
 
    
-//when get the right answer, add modal and then go to next question
+//when get the right answer, add modal and then go to next questionDiv
 
 //
 
@@ -169,7 +187,9 @@ streak.innerHTML="Your total score is " + total
 //include high score!
 
 //more things to do...
-//high score, cooler css (button animation), more questions, fix the winning streak and total score functionality, figure out modals, and maybe try to do more than one page? 
+//high score, cooler css (button animation), more questionDivs, fix the winning streak and total score functionality, figure out modals, and maybe try to do more than one page?
+// progress back with number correct and shows progress visually
+// high score list
 
 //questions
 // why does the backgruond image turn blue only for a second? Page reloading? 
